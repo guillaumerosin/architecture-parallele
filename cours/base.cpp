67bbd2ca55ddac579f4a7f56fe640845531829b7
guillaumerosin <guillaumerosin@users.noreply.github.com>
2026-03-10 16:35:56 +0100

    up

--- cours/base.cpp
+++ cours/base.cpp
@@ -1,9 +1,21 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
+
 
 int main() {
+
+    int N;
     std::cout << "Il était une fois, dans un monde sans Bryan\n";
-    std::cout << "Un monde stable..."
+    std::cout << "Un monde stable... équilibré...  \n";
     std::cout << "Entre une valeur entiere N";
-    return 0;
+    std::cin >> N;
+    std::cout << "N =" << N << std::endl;
+    
+    
+    std::vector<int> BRYAN_IS_IN_THE_KITCHEN(N * N);  //Création de ma matrice
+    M[i * N + j] = 3;
+    int x = M[i * N + j];
+
+    
 }
